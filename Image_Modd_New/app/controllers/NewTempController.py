d847b61509f28d3a3e7e60efccfd283e8c6b9ef7
Steve <stevensu1188@gmail.com>
2025-04-18 15:33:19 -0400

    edit NewTempalte

--- Image_Modd_New/app/controllers/NewTempController.py
+++ Image_Modd_New/app/controllers/NewTempController.py
@@ -1,3 +1,5 @@
+from tkinter import filedialog
+
 class NewTempController:
     def __init__(self, model, view):
         self.model = model
@@ -6,5 +8,10 @@ class NewTempController:
         self._bind()
 
     def _bind(self):
-        self.frame.home_btn.config(command = lambda: self.view.switch("home"))
+        self.frame.home_button.config(command = lambda: self.view.switch("home"))
+        self.frame.image_button.config(command = self.select_image)
         pass
+    def select_image(self):
+        file_path = filedialog.askopenfilename(title="Select an Image", filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif")])
+        if file_path:
+            self.image_Entry.config(text=file_path)
--- Image_Modd_New/app/views/NewTempView.py
+++ Image_Modd_New/app/views/NewTempView.py
@@ -1,4 +1,4 @@
-from tkinter import Frame, Label, Button
+from tkinter import Frame, Label, Button, Entry
 
 class NewTemplateView(Frame):
     def __init__(self, *args, **kwargs):
@@ -9,8 +9,19 @@ class NewTemplateView(Frame):
         self.header = Label(self, text="New Template")
         self.header.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
 
-        self.home_btn = Button(self, text="home")
-        self.home_btn.grid(row=2, column=0, padx=10, pady=10)
+        self.folder_name_entry = Entry(self)
+        self.folder_name_entry.grid(row=1, column=0, padx = 10, pady = 10)
 
-        self.cfg_template_btn = Button(self, text="Modify Template")
-        self.cfg_template_btn.grid(row=3, column=0, padx=10, pady=10)
+        self.image_button = Button(self, text="Select Image File")
+        self.image_button.grid(row=2, column=0, columnspan=1, pady=10)
+        
+        Label(self, text="New File Name:").grid(row=1, column=0, padx=10, pady=10)
+        self.image_Entry = Entry(self)
+        self.image_Entry.grid(row=1, column=1, padx=10, pady=10)       
+        
+        self.new_template_button = Button(self, text="Modify Template")
+        self.new_template_button.grid(row=3, column=0, padx=10, pady=10)
+        
+        self.home_button = Button(self, text="home")
+        self.home_button.grid(row=4, column=0, padx=10, pady=10)
+
